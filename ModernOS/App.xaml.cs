30a4b7116ef46002e904a39de9bc55755f6ea80f
Niels Laute <niels.laute@live.nl>
2019-09-27 11:49:03 +0200

    Commit

--- ModernOS/App.xaml.cs
+++ ModernOS/App.xaml.cs
@@ -26,10 +26,8 @@ namespace ModernOS
     /// </summary>
     sealed partial class App : Application
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
+
+    
         public App()
         {
             this.InitializeComponent();
